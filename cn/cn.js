6950c634a2c5824ae57f21eefcb69139f30a325e
Azlan <98301786+azlanajju@users.noreply.github.com>
2022-10-25 19:57:50 +0530

    Update cn.js

--- cn/cn.js
+++ cn/cn.js
@@ -6,3 +6,29 @@ function ifno(){
   document.getElementById('confirm').style.display="none";
   
 }
+function myFunction1() {
+  
+ document.getElementById("copied").innerHTML = "Copied!";
+
+  var copyText = document.getElementById("myInput");
+  copyText.select(); 
+  navigator.clipboard.writeText(copyText.value);
+}
+
+function myFunction2() {
+  
+ document.getElementById("copied1").innerHTML = "Copied!";
+
+  var copyText = document.getElementById("myInput1");
+  copyText.select(); 
+  navigator.clipboard.writeText(copyText.value);
+}
+
+function myFunction3() {
+  
+ document.getElementById("copied2").innerHTML = "Copied!";
+
+  var copyText = document.getElementById("myInput2");
+  copyText.select(); 
+  navigator.clipboard.writeText(copyText.value);
+}
