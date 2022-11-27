d5831e9267cfb0e9cc2befb7878ce4d922ce424e
Azlan <98301786+azlanajju@users.noreply.github.com>
2022-11-27 07:38:26 +0530

    Update cn.js

--- cn/cn.js
+++ cn/cn.js
@@ -54,3 +54,31 @@ function myFunction4() {
    copyText.select(); 
    navigator.clipboard.writeText(copyText.value);
  }
+ function myFunction7() {
+  document.getElementById("copied6").innerHTML = "Copied!";
+ 
+   var copyText = document.getElementById("myInput6");
+   copyText.select(); 
+   navigator.clipboard.writeText(copyText.value);
+ }
+ function myFunction8() {
+  document.getElementById("copied7").innerHTML = "Copied!";
+ 
+   var copyText = document.getElementById("myInput7");
+   copyText.select(); 
+   navigator.clipboard.writeText(copyText.value);
+ }
+ function myFunction9() {
+  document.getElementById("copied8").innerHTML = "Copied!";
+ 
+   var copyText = document.getElementById("myInput8");
+   copyText.select(); 
+   navigator.clipboard.writeText(copyText.value);
+ }
+ function myFunction10() {
+  document.getElementById("copied9").innerHTML = "Copied!";
+ 
+   var copyText = document.getElementById("myInput9");
+   copyText.select(); 
+   navigator.clipboard.writeText(copyText.value);
+ }
